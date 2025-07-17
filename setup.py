9f290b825bd493d900385e6967faf1a0cefe5a71
jkmulq <jmulqueeney@uchicago.edu>
2025-07-17 10:09:37 -0500

    loading dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-#setup
+from setuptools import setup, find_packages
+
+setup(
+    name='ustat_var',               
+    version='0.1.0',                 
+    description='Unbiased estimators for variance of teacher effects',
+    packages=find_packages(),        
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    python_requires='>=3.7',        
+)
